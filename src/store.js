95e8536778427ef36ab433d8ab08c0acfdb79065
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-06-21 05:37:52 +0700

    ADD | Vuex State

---
+++ src/store.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+const Home = {
+  namespaced: true,
+  state: {
+    data: {
+      apiKey: 'f8fd87d48cf746e0a817a4f7a21bafe4',
+      news: []
+    }
+  },
+  actions: {
+    getNews ({ state }) {
+      axios.get(`https://newsapi.org/v2/everything?q=technology&sortBy=publishedAt&apiKey=${state.data.apiKey}`)
+        .then(response => {
+          state.data.news = response.data.articles
+        })
+    }
+  }
+}
+
+export default new Vuex.Store({
+  modules: {
+    home: Home
+  }
+})
