82f937a861b1582cddb950974961058e640f80b6
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-06-21 05:57:39 +0700

    ADD | Home Fetch Api Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col-lg-6 col-xs-12 media" v-for="(item, index) in news" :key="index">
+      <img class="mr-3 mt-2" width="40%" :src="item.urlToImage" alt="Generic placeholder image">
+      <div class="media-body">
+        <h6 class="mt-0"><b>{{item.title}}</b></h6>
+        {{item.description}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: 'Home',
+  mounted () {
+    this.$store.dispatch('home/getNews')
+  },
+  computed: {
+    news () {
+      return this.$store.state.home.data.news
+    }
   }
 }
 </script>
